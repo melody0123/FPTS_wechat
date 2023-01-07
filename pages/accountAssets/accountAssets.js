5102cf9a71fcb27baba2e092ae6e2359caac8690
LiZiYang3 <2495131032@qq.com>
2023-01-08 01:30:50 +0800

    修改了交易记录和

--- pages/accountAssets/accountAssets.js
+++ pages/accountAssets/accountAssets.js
@@ -1,19 +1,76 @@
-// pages/accountAssets/accountAssets.js
+// pages/todolist/todolist.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    list: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    var that=this;
+    wx.request({
+      url: 'http://localhost/assets/account_assets/getById',
+      method:'POST',
+      data:{userId: 1, accountId: null},
+      header:{
+        'content-type': 'application/x-www-form-urlencoded;charset=UTF-8', // 请求头
+        'cookie': wx.getStorageSync('sessionId')
+      },
+      success:function(res){ 
+        var list=res.data; 
+        if(list==null){ 
+          var toastText='该用户未创建账户';
+          wx.showToast({
+            title: toastText,
+            icon:'error',
+            duration:2000 //弹出时间
+          })
+        }else{
+          console.log(res.data);
+          that.setData({
+            list:list
+          })
+        }
+      }
+    })
   },
+
+  increase: function(e){
+    wx.setStorageSync('flag', true);
+    wx.setStorageSync('accountId', e.currentTarget.dataset.accountId);
+    wx.setStorageSync('userId', e.currentTarget.dataset.userId);
+    wx.navigateTo({
+      url: '/pages/assetsEdit/assetsEdit',
+    })
+  },
+  decrease: function(e){
+    wx.setStorageSync('flag', false);
+    wx.setStorageSync('accountId', e.currentTarget.dataset.accountId);
+    wx.setStorageSync('userId', e.currentTarget.dataset.userId);
+    wx.navigateTo({
+      url: '/pages/assetsEdit/assetsEdit',
+    })
+  },
+
+  totalAssets: function(){
+    var length = this.data.list.length;
+    var i = 0;
+    var totalAssets = 0;
+    for (;i<length;i++){
+      totalAssets += this.data.list[i];
+    }
+    wx.showToast({
+      title: '您的账户总资产为'+totalAssets,
+      icon: '',
+      duration: 2000
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
--- pages/accountAssets/assetsEdit.js
+++ pages/accountAssets/assetsEdit.js
@@ -1,66 +1,145 @@
 // pages/accountAssets/assetsEdit.js
 Page({
-
+ 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userId: wx.getStorageSync('userId'),
+    accountId: wx.getStorageSync('accountId'),
+    flag: wx.getStorageSync('flag'),
+    modalHidden: true,
+    balance: null,
+    changeBalance: null
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+  //表单提交
+  formSubmit(e){
+    var information= e.detail.value;
 
+    this.setData({
+      information: e.detail.value,
+      modalHidden:false
+    });
+  },
+ 
+  //模态框取消
+  modalCancel(){
+    wx.showToast({
+      title: '取消提交',
+      icon:'none'
+    })
+    this.setData({
+      modalHidden:true,
+    })
+  },
+  //模态框确定
+  modalConfirm() {
+    this.saveEdit();
+    wx.showToast({
+      title: '提交成功',
+      icon:'success'
+    })
+    this.setData({
+      modalHidden: true
+    })
+    wx.navigateTo({
+      url: '/pages/accountAssets/accountAssets',
+    })
+  },
+  onLoad: function (options) {
+    this.showAssets();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  showAssets: function(){
+    var that=this;
+    var userId =  wx.getStorageSync('userId');
+    var accountId = wx.getStorageSync('accontId');
+    var flag = wx.getStorageSync('flag');
+    wx.request({
+      url: 'http://localhost/assets/account_assets/getById',
+      method:'POST',
+      data:{userId: userId, accountId: accountId},
+      header:{
+        'content-type': 'application/x-www-form-urlencoded;charset=UTF-8', // 请求头
+        'cookie': wx.getStorageSync('sessionId')
+      },
+      success:function(res){ 
+        that.setData({
+          balance: res.data[0].accountBalance
+        })
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  saveEdit: function(){
+    var that=this;
+    if (that.data.flag){
+      wx.request({
+        url: 'http://localhost/assets/account_assets/increase',
+        method:'POST',
+        data:{
+          userId: that.data.userId,
+          accountId: that.data.accountId,
+          assets: that.data.changeBalance
+        },
+        header:{
+          'content-type': 'application/x-www-form-urlencoded;charset=UTF-8', // 请求头
+          'cookie': wx.getStorageSync('sessionId')
+        },
+        success:function(res){ 
+          var result=res.data.msg;
+          console.log(result); 
+          var toastText="操作成功"; 
+          if(result!= "操作成功"){
+            toastText = "操作失败";
+          }else{
+            that.onLoad();
+          }
+          wx.showToast({
+            title: toastText,
+            icon:'',
+            duration:2000 });
+        }
+      })
+    }
+    else{
+      if (that.data.balance >= that.data.changeBalance){
+        wx.request({
+          url: 'http://localhost/assets/account_assets/decrease',
+          method:'POST',
+          data:{
+            userId: that.data.userId,
+            accountId: that.data.accountId,
+            assets: that.data.changeBalance
+          },
+          header:{
+            'content-type': 'application/x-www-form-urlencoded;charset=UTF-8', // 请求头
+            'cookie': wx.getStorageSync('sessionId')
+          },
+          success:function(res){ 
+            var result=res.data.msg;
+            console.log(result); 
+            var toastText="操作成功"; 
+            if(result!= "操作成功"){
+              toastText = "操作失败";
+            }else{
+              that.onLoad();
+            }
+            wx.showToast({
+              title: toastText,
+              icon:'',
+              duration:2000 });
+          }
+        })
+      }
+      else{
+        wx.showToast({
+          title: '账户余额不足',
+          icon:'error',
+          duration:2000 });
+      }
+    }
   }
+
 })
