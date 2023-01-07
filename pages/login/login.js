3e5986d62a83291d833f0fd7c6cd9865a3571c76
melody0123 <88380000+melody0123@users.noreply.github.com>
2023-01-07 21:03:53 +0800

    给登录界面添加了标题 // Add the title for login page
    1. 给登录界面添加了标题
    2. 规范化代码，把所有style移动到css文件中，并修改了一些函数的写法
    3. 修改了 @laybxc 的自动登录。修改后数据会显式地给出，避免发布正式版本时忘记删去。
    ---
    1. Add the title for login page.
    2. Normalize the code. Move all "style" properties from .wxml files to .wxss files. Modify the definitions of some functions
    3. Modify auto-fill function provided by @laybxc . After modifying, the form data will be displayed explicitly, which can help avoid forgetting to remove it before releasing public versions.

--- pages/login/login.js
+++ pages/login/login.js
@@ -11,31 +11,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-    username:'admin',
-    password:'admin123',  //一键登录
+    username:'',
+    password:'', 
     validateCode:'',
     imgUrl: ''
   },
-  setUsername(e) {
-    // 同步输入框数据到 data
-    this.setData({
-      username: e.detail.value
-    })
-  },
-  setPassword(e) {
-    // 同步输入框数据到 data
-    this.setData({
-      password: e.detail.value
-    })
-  },
-  setValidateCode(e) {
-    // 同步输入框数据到 data
-    this.setData({
-      validateCode:e.detail.value
-    })
-  },
-  loginFun(){
-    // console.log(this.data.formData);
+  loginFun: function(e) {
+    // console.log(e.detail.value);
+    // 读取一边用户输入的数据
+    this.data.username = e.detail.value.username;
+    this.data.password = e.detail.value.password;
+    this.data.validateCode = e.detail.value.validateCode;
     // 检查所有登录用的字段是否填写完毕
     if(!this.data.username || !this.data.password || !this.data.validateCode) {
       // 没有填写完整
@@ -55,12 +41,12 @@ Page({
           'content-type': 'application/x-www-form-urlencoded;charset=UTF-8', // 请求头
           'cookie': wx.getStorageSync('sessionId') // 取 session
         },
-        success (res) {
-          console.log(res);
+        success: function(res) {
+          // console.log(res);
           // 判断是否登录成功
           if (res.statusCode == 200 && res.data.code == 0 && res.data.msg == "操作成功") {
             // 登录成功
-            console.log("登录成功");
+            // console.log("登录成功");
             wx.showModal({
               title: '登录成功',
               content: '即将前往首页',
@@ -73,7 +59,7 @@ Page({
             })
           } else if (res.statusCode == 200 && res.data.code == 500) {
             // 登录失败
-            console.log("登录失败，" + res.data.msg);
+            // console.log("登录失败，" + res.data.msg);
             wx.showModal({
               title: '登录失败',
               content: res.data.msg + '，请重试',
@@ -86,9 +72,9 @@ Page({
             })
           }
         },
-        fail(res) {
+        fail: function(res) {
           // 网络错误
-          console.log("网络错误");
+          // console.log("网络错误");
           wx.showModal({
             title: '登录失败',
             content: '网络错误，请重试',
@@ -107,7 +93,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
+  onLoad: function(options) {
     let that = this;
     wx.request({
       url: 'http://localhost/login',
@@ -117,7 +103,7 @@ Page({
      url: 'http://localhost/captcha/captchaImage?type=math',
      method: 'GET',
      responseType: 'arraybuffer',
-     success (res) {
+     success: function(res) {
        // 记录 session
        let sessionId = (res.header['Set-Cookie'].split('; '))[0];
        // console.log(sessionId);
