ee01a7179568f87753554bcd8a975f0ce29ff49f
Guoxi-Zhang <79048941+Guoxi-Zhang@users.noreply.github.com>
2023-01-07 19:18:52 +0800

    完成财经新闻模块

---
+++ pages/news/news.js
@@ -0,0 +1,141 @@
+//index.js
+
+var util = require('../../utils/util.js')
+var app = getApp()
+Page({
+  data: {
+    newsList:[],
+    feed: [],
+    feed_length: 0
+  },
+  //事件处理函数
+  bindItemTap: function(e) {
+    var link = e.currentTarget.dataset.link;
+    console.log(link);
+    app.globalData.newsSourceLink = link;
+    wx.navigateTo({
+      url: '/pages/news/newsSource'
+    })
+  },
+  
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    this.searchNewsData();
+    //调用应用实例的方法获取全局数据
+    this.getData();
+  },
+  upper: function () {
+    wx.showNavigationBarLoading()
+    this.refresh();
+    console.log("upper");
+    setTimeout(function(){wx.hideNavigationBarLoading();wx.stopPullDownRefresh();}, 2000);
+  },
+  lower: function (e) {
+    wx.showNavigationBarLoading();
+    var that = this;
+    setTimeout(function(){wx.hideNavigationBarLoading();that.nextLoad();}, 1000);
+    console.log("lower")
+  },
+  //scroll: function (e) {
+  //  console.log("scroll")
+  //},
+
+  //网络请求数据, 实现首页刷新
+  refresh0: function(){
+    var index_api = '';
+    util.getData(index_api)
+        .then(function(data){
+          //this.setData({
+          //
+          //});
+          console.log(data);
+        });
+  },
+
+  //使用本地 fake 数据实现刷新效果
+  getData: function(){
+    var feed = util.getData2();
+    console.log("loaddata");
+    var feed_data = feed.data;
+    this.setData({
+      feed:feed_data,
+      feed_length: feed_data.length
+    });
+  },
+  refresh: function(){
+    wx.showToast({
+      title: '刷新中',
+      icon: 'loading',
+      duration: 3000
+    });
+    var feed = util.getData2();
+    console.log("loaddata");
+    var feed_data = feed.data;
+    this.setData({
+      feed:feed_data,
+      feed_length: feed_data.length
+    });
+    setTimeout(function(){
+      wx.showToast({
+        title: '刷新成功',
+        icon: 'success',
+        duration: 2000
+      })
+    },3000)
+
+  },
+
+  //使用本地 fake 数据实现继续加载效果
+  nextLoad: function(){
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 4000
+    })
+    var next = util.getNext();
+    console.log("continueload");
+    var next_data = next.data;
+    this.setData({
+      feed: this.data.feed.concat(next_data),
+      feed_length: this.data.feed_length + next_data.length
+    });
+    setTimeout(function(){
+      wx.showToast({
+        title: '加载成功',
+        icon: 'success',
+        duration: 2000
+      })
+    },3000)
+  },
+  searchNewsData: function(){
+    var that=this;
+    console.log(wx.getStorageSync('sessionId'));
+    wx.request({
+      url: 'http://localhost/finance_news/news_manage/wxGetNews',
+      method:'GET',
+      data:{},
+      header:{
+        'content-type': 'application/x-www-form-urlencoded;charset=UTF-8', // 请求头
+        'cookie': wx.getStorageSync('sessionId')
+      },
+      success:function(res){ 
+        var list=res.data; 
+        if(list==null){ 
+          var toastText='获取数据失败';
+          wx.showToast({
+            title: toastText,
+            icon:'',
+            duration:2000 //弹出时间
+          })
+        }else{
+          console.log(res.data);
+          that.setData({
+            newsList:list
+          })
+        }
+      }
+    })
+  }
+
+})
