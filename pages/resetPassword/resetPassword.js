e327692e706db99cfd16bd9b9ca2d0273fae023f
melody0123 <88380000+melody0123@users.noreply.github.com>
2023-01-09 01:50:59 +0800

    完成重置密码邮箱输入页的前端实现 / Implemented the front end of email input page of password reset function

--- pages/resetPassword/resetPassword.js
+++ pages/resetPassword/resetPassword.js
@@ -19,15 +19,14 @@ Page({
     imgUrl: ''
   },
 
-  // 登录函数
-  loginFun: function(e) {
+  // 提交重置密码申请
+  submitResetRequest: function(e) {
     // console.log(e.detail.value);
     // 读取一边用户输入的数据
-    this.data.username = e.detail.value.username;
-    this.data.password = e.detail.value.password;
+    this.data.username = e.detail.value.mailAddress;
     this.data.validateCode = e.detail.value.validateCode;
-    // 检查所有登录用的字段是否填写完毕
-    if(!this.data.username || !this.data.password || !this.data.validateCode) {
+    // 检查所有字段是否填写完毕
+    if(!this.data.mailAddress || !this.data.validateCode) {
       // 没有填写完整
       wx.showToast({
         title: '信息不完整',
@@ -35,11 +34,11 @@ Page({
         duration: 2000
       })
     } else {
-      // 填写完整，向服务器发送登录请求
+      // 填写完整，向服务器发送重置请求
       let that = this;
       wx.request({
-        url: 'http://' + app.globalData.serverIP + '/login', //请求地址
-        data: {username: that.data.username, password: that.data.password, validateCode: that.data.validateCode, rememberMe: "false"}, //请求数据
+        url: 'http://' + app.globalData.serverIP + '/resetPasswordRequest', //请求地址
+        data: {mailAddress: that.data.mailAddress, validateCode: that.data.validateCode}, //请求数据
         method: 'POST', //请求方法
         header: {
           'content-type': 'application/x-www-form-urlencoded;charset=UTF-8', // 请求头
@@ -47,45 +46,30 @@ Page({
         },
         success: function(res) {
           // console.log(res);
-          // 判断是否登录成功
+          // 判断是否请求是否成功
           if (res.statusCode == 200 && res.data.code == 0 && res.data.msg == "操作成功") {
-            // 登录成功
-            // console.log("登录成功");
+            // 操作成功
+            // console.log("操作成功");
             wx.showModal({
-              title: '登录成功',
-              content: '即将前往首页',
+              title: '操作成功',
+              content: '即将前往重置页面',
               showCancel: false,
               complete: (res) => {
                 wx.switchTab({
-                  url: '/pages/news/news',
+                  url: '/pages/setNewPassword/setNewPassword',
                 })
               }
             });
-            // 顺便获取用户详细信息
-            wx.request({
-              url: 'http://' + app.globalData.serverIP + '/system/user/getUserInfo',
-              method: 'POST',
-              header: {
-                'cookie': wx.getStorageSync('sessionId'),
-                'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
-              },
-              data: {
-                'loginName': that.data.username
-              },
-              success: function(res) {
-                app.globalData.sysUserInfo = res.data; // 存入全局变量
-              }
-            });
           } else if (res.statusCode == 200 && res.data.code == 500) {
-            // 登录失败
+            // 操作失败
             // console.log("登录失败，" + res.data.msg);
             wx.showModal({
-              title: '登录失败',
+              title: '操作失败',
               content: res.data.msg + '，请重试',
               showCancel: false,
               complete: (res) => {
                 wx.reLaunch({
-                  url: '/pages/login/login',
+                  url: '/pages/resetPassword/resetPassword',
                 })
               }
             })
@@ -95,12 +79,12 @@ Page({
           // 网络错误
           // console.log("网络错误");
           wx.showModal({
-            title: '登录失败',
+            title: '操作失败',
             content: '网络错误，请重试',
             showCancel: false,
             complete: (res) => {
               wx.reLaunch({
-                url: '/pages/login/login',
+                url: '/pages/resetPassword/resetPassword',
               })
             }
           })
