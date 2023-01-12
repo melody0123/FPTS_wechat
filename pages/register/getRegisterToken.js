7da447d7f479aba1bbc3f3823fb5801711c96d0b
melody0123 <88380000+melody0123@users.noreply.github.com>
2023-01-13 05:18:25 +0800

    完成注册中填写邮箱的步骤

--- pages/register/getRegisterToken.js
+++ pages/register/getRegisterToken.js
@@ -1,5 +1,5 @@
-// pages/login/login.js
-// pages/login/index.js
+// pages/register/getRegisterToken.js
+// pages/resetPassword/resetPassword.js
 var app = getApp();
 import { btoa } from '../../utils/imageUtils';
 const arrayBufferToBase64Img = (arrayBuffer) => {
@@ -20,21 +20,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-    username:'',
-    password:'', 
+    mailAddress:'',
     validateCode:'',
     imgUrl: ''
   },
 
-  // 登录函数
-  loginFun: function(e) {
+  // 提交注册申请
+  submitResetRequest: function(e) {
     // console.log(e.detail.value);
+    let that = this;
     // 读取一边用户输入的数据
-    this.data.username = e.detail.value.username;
-    this.data.password = e.detail.value.password;
+    this.data.mailAddress = e.detail.value.mailAddress;
     this.data.validateCode = e.detail.value.validateCode;
-    // 检查所有登录用的字段是否填写完毕
-    if(!this.data.username || !this.data.password || !this.data.validateCode) {
+    // 检查所有字段是否填写完毕
+    if(!this.data.mailAddress || !this.data.validateCode) {
       // 没有填写完整
       wx.showToast({
         title: '信息不完整',
@@ -42,11 +41,10 @@ Page({
         duration: 2000
       })
     } else {
-      // 填写完整，向服务器发送登录请求
-      let that = this;
+      // 填写完整，向服务器发送重置请求
       wx.request({
-        url: 'http://' + app.globalData.serverIP + '/login', //请求地址
-        data: {username: that.data.username, password: that.data.password, validateCode: that.data.validateCode, rememberMe: "false"}, //请求数据
+        url: 'http://' + app.globalData.serverIP + '/getRegisterToken', //请求地址
+        data: {email: that.data.mailAddress, validateCode: that.data.validateCode}, //请求数据
         method: 'POST', //请求方法
         header: {
           'content-type': 'application/x-www-form-urlencoded;charset=UTF-8', // 请求头
@@ -54,47 +52,32 @@ Page({
         },
         success: function(res) {
           // console.log(res);
-          // 判断是否登录成功
+          // 判断是否请求是否成功
           if (res.statusCode == 200 && res.data.code == 0 && res.data.msg == "操作成功") {
-            // 登录成功
-            // console.log("登录成功");
+            // 操作成功
+            // console.log("操作成功");
             wx.showModal({
-              title: '登录成功',
-              content: '即将前往首页',
+              title: '操作成功',
+              content: '令牌已经发往您的邮箱，即将前往信息填写页面',
               showCancel: false,
               complete: (res) => {
-                wx.switchTab({
-                  url: '/pages/news/news',
+                wx.navigateTo({
+                  url: '/pages/register/register',
                 })
-              }
-            });
-            // 顺便获取用户详细信息
-            wx.request({
-              url: 'http://' + app.globalData.serverIP + '/system/user/getUserInfo',
-              method: 'POST',
-              header: {
-                'cookie': wx.getStorageSync('sessionId'),
-                'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
-              },
-              data: {
-                'loginName': that.data.username
-              },
-              success: function(res) {
-                app.globalData.sysUserInfo = res.data; // 存入全局变量
-                // console.log(app.globalData.sysUserInfo);
               }
             });
           } else if (res.statusCode == 200 && res.data.code == 500) {
-            // 登录失败
+            // 操作失败
             // console.log("登录失败，" + res.data.msg);
             wx.showModal({
-              title: '登录失败',
+              title: '操作失败',
               content: res.data.msg + '，请重试',
               showCancel: false,
-              complete: (res) => {
-                wx.reLaunch({
-                  url: '/pages/login/login',
-                })
+              complete: function() {
+                that.refreshValidateCode();
+                that.setData({
+                  validateCode: ''
+                });
               }
             })
           }
@@ -103,32 +86,27 @@ Page({
           // 网络错误
           // console.log("网络错误");
           wx.showModal({
-            title: '登录失败',
+            title: '操作失败',
             content: '网络错误，请重试',
             showCancel: false,
-            complete: (res) => {
-              wx.reLaunch({
-                url: '/pages/login/login',
-              })
+            complete: function() {
+              that.refreshValidateCode();
+              that.setData({
+                validateCode: ''
+              });
             }
           })
         }
       })
     }
   },
-  
-  // 跳转到注册页面
-  toRegisterPage: function() {
-    wx.navigateTo({
-      url: '/pages/register/getRegisterToken',
-    })
-  },
 
-  // 跳转到短信验证码登录页面
-  toResetPage: function() {
-    wx.navigateTo({
-      url: '/pages/resetPassword/resetPassword',
-    })
+  // 监听验证码输入框
+  getChar: function(e) {
+    // console.log(e.detail.value);
+    this.setData({
+      validateCode:e.detail.value
+    });
   },
 
   // 刷新验证码
